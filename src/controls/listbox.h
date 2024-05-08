495296fe120436f4b87855fc9ad64d4291d91808
Dmitry Tretyakov <tretdm@localhost.localdomain>
2024-05-08 18:23:33 +0700

    Improve file manager navigation

--- src/controls/listbox.h
+++ src/controls/listbox.h
@@ -3,21 +3,32 @@
 
 #include "extwnd.h"
 #include "../utils/uictrl.h"
+#include "../utils/extstr.h"
 #include <ncurses.h>
+
+struct ListItem {
+    char title[128];
+    void* ptr;
+};
 
 class ListBoxCtrl : UIControl {
     public:
         ListBoxCtrl(ExtWindowCtrl* pParent, int pItemCount);
+        ~ListBoxCtrl();
+        void addListItem(int index, ListItem* item);
         int getSelectionIndex();
         void setSelectionIndex(int index);
         void onKeyPressed(char k);
         void drawListPointer(int x, int y, bool isVisible);
+        void goToPage(int pPageNumber);
+        void recreate(int pItemCount);
         int getItemCount();
-        void setItemCount(int count);
         int hX, hY, hWidth, hHeight;
+        struct ListItem**   gListItems;
     protected:
+        int getVirtualSelectionIndex();
         ExtWindowCtrl* gParent;
-        int gSelectionIndex, gItemCount;
+        int gSelectionIndex, gPageNumber, gItemCount;
 
 };
 
--- src/opendss.cpp
+++ src/opendss.cpp
@@ -18,11 +18,13 @@
 #include "interfaces/fileman.h"
 #include "interfaces/pguiman.h"
 
+#define MAX_FILENAME_LENGTH 24
+
 class IOpenDSSFileManager : IFileManager {
     public:
         void onError(int cmdId, int errorCode);
         void onResult(int cmdId, int resultCode);
-        void onDirectoryRead(struct dirent* ent, int index);
+        void onDirectoryRead(dirent** ents);
 };
 
 IOpenDSSFileManager         *gFileManInterface;
@@ -45,12 +47,6 @@ void openFileManager() {
     char wndTitle[] = "File Manager";
     gFileManWnd = gPsGuiMan->createWindow(wndTitle, -2, -2, true);
     sprintf(gFileManWnd->id, "fm_wnd");
-    ListBoxCtrl *mFileListBox = new ListBoxCtrl(gFileManWnd, (int)gFileMan->getFilesCount());
-    mFileListBox->setSelectionIndex(0);
-    mFileListBox->hY = 4;
-    mFileListBox->hX = 2;
-    mFileListBox->hHeight = gFileManWnd->hHeight - 6;
-    gFileManWnd->addControl((UIControl*)mFileListBox);
     gFileMan->readCurrentDir();
 }
 
@@ -91,18 +87,40 @@ void IOpenDSSFileManager::onResult(int cmdId, int resultCode) {
 
 /* Handles File Manager directory list. */
 
-void IOpenDSSFileManager::onDirectoryRead(struct dirent* ent, int index) {
-    char short_fname[255];
-    ListBoxCtrl* mFileListBox = ((ListBoxCtrl*)gFileManWnd->hCtrls[0]);
-    int hY = mFileListBox->hY;
-    if(index <= mFileListBox->hHeight)  {
-        sprintf(short_fname, "%s", ent->d_name);
-        ExtString::strcut(short_fname, 32, -1);
-        mvwprintw(gFileManWnd->hWnd, index + hY, 4, "%s", short_fname);
-        if(ExtString::strendq(ent->d_name, ".mp3")) {
-            mvwprintw(gFileManWnd->hWnd, index + hY, 38, "0:00:00.000");
-        }
+void IOpenDSSFileManager::onDirectoryRead(dirent** ents) {
+    ListBoxCtrl *mFileListBox;
+    if(!gFileManWnd->hCtrls[0]) {
+        mFileListBox = new ListBoxCtrl(gFileManWnd, gFileMan->getFilesCount());
+        mFileListBox->setSelectionIndex(0);
+        mFileListBox->hY = 4;
+        mFileListBox->hX = 2;
+        mFileListBox->hHeight = gFileManWnd->hHeight - 6;
+
+        gFileManWnd->addControl((UIControl*)mFileListBox);
+    } else {
+        mFileListBox = ((ListBoxCtrl*) gFileManWnd->hCtrls[0]);
+        mFileListBox->recreate(gFileMan->getFilesCount());
     }
+
+    for(int i = 0; i < gFileMan->getFilesCount(); i++) {
+        ListItem* item = new ListItem();
+        sprintf(item->title, "%s", ents[i]->d_name);
+        if(strlen(item->title) > MAX_FILENAME_LENGTH) {
+            ExtString::strcut(item->title, MAX_FILENAME_LENGTH - 3, -1);
+            sprintf(item->title + MAX_FILENAME_LENGTH - 3, "...");
+        }
+        if(i <= mFileListBox->hHeight
+            && ExtString::strendq(ents[i]->d_name, ".mp3"))  {
+            mvwprintw(
+                gFileManWnd->hWnd,
+                i + mFileListBox->hY,
+                MAX_FILENAME_LENGTH + 6,
+                "0:00:00.000"
+            );
+        }
+        mFileListBox->addListItem(i, item);
+    }
+
     wrefresh(gFileManWnd->hWnd);
 }
 
@@ -111,21 +129,20 @@ void IOpenDSSFileManager::onDirectoryRead(struct dirent* ent, int index) {
 void IOpenDSSPseudoGUIManager::onKeyPressed(char k, ExtWindowCtrl* pExtWnd) {
     if((int)k == 2 || (int)k == 3) {
         ListBoxCtrl* mFileListBox = ((ListBoxCtrl*)gFileManWnd->hCtrls[0]);
-        mFileListBox->setItemCount(gFileMan->getFilesCount());
         if(strcmp(pExtWnd->id, "fm_wnd") == 0) {
             if(gFileManWnd->getControlsSize() > 0) {
-                ((ListBoxCtrl*)gFileManWnd->hCtrls[0])->onKeyPressed(k);
+                mFileListBox->onKeyPressed(k);
             }
         }
     } else if((int)k == 10) { // ENTER key
+        ListBoxCtrl* mFileListBox = ((ListBoxCtrl*)gFileManWnd->hCtrls[0]);
         dirent* ent = gFileMan->getFile(
-            ((ListBoxCtrl*)gFileManWnd->hCtrls[0])->getSelectionIndex()
+            mFileListBox->getSelectionIndex()
         );
         char fname[255];
         sprintf(fname, "%s/%s", gFileMan->getCurrentPath(), ent->d_name);
         if(ent->d_type == 4) { // if it's directory
             gFileMan->readDir(fname);
-            ((ListBoxCtrl*)gFileManWnd->hCtrls[0])->setItemCount((int)gFileMan->getFilesCount());
         }
     }
 
