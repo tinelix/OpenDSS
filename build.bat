b57d04594eb4b10ea83fd674632bc1fa04c39566
Dmitry Tretyakov <tretdm@inbox.ru>
2024-06-09 09:10:08 +0700

    Update project

--- build.bat
+++ build.bat
@@ -7,6 +7,6 @@ CD out\libs\pdcurses
 
 SET PDCURSES_SRCDIR=%PROJECT_PATH%\libs\pdcurses
 
-NMAKE %PROJECT_PATH%\libs\pdcurses\win32\vcwin32.mak WIDE=Y DLL=Y
+NMAKE %PROJECT_PATH%\libs\pdcurses\win32\vcwin32.mak DLL=Y
 
 CD ..\..\..
