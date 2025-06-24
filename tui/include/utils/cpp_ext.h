76662318cb3d5c5c3f17c0c9f58dfb0074f6e620
Dmitry Tretyakov <tretdm@inbox.ru>
2025-06-24 12:06:59 +0700

    Rewrite source code for VC 2.0

---
+++ tui/include/utils/cpp_ext.h
@@ -0,0 +1,12 @@
+#ifndef _CPP_EXT
+#define _CPP_EXT
+
+
+#if _MSC_VER = 800		// Microsoft Visual C++ 2.0 (1994)
+	typedef unsigned char bool;
+	#define true ((bool)1)
+	#define false ((bool)0)
+#endif
+
+
+#endif
